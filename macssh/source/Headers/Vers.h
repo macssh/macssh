c2197aab9b985d0f935506a04677e329ace858c7
chombier <>
2001-04-17 18:36:49 +0000

    2.1b3 update

--- macssh/source/Headers/Vers.h
+++ macssh/source/Headers/Vers.h
@@ -4,7 +4,7 @@
 #define VMajor			2
 #define VMinor			(1 * 16) + 0
 #define VStage			beta
-#define VRelease		2
-#define VShortString	"2.1b2"
+#define VRelease		3
+#define VShortString	"2.1b3"
 
 #define	CopyrightString	"©2000-2001 J.-P. Stierlin"
