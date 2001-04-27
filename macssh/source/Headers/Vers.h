495feda37f355c3c138edf314bba26fba9d4ed28
chombier <>
2001-04-27 15:40:25 +0000

    #ifdef REZ, 2.1b5 update

--- macssh/source/Headers/Vers.h
+++ macssh/source/Headers/Vers.h
@@ -1,10 +1,17 @@
+#if REZ_COMPILER
 #include "Types.r"
 #include "SysTypes.r"
+#else
+#define	development	0x20
+#define	alpha 0x40
+#define	beta 0x60
+#define	final 0x80
+#endif
 
 #define VMajor			2
 #define VMinor			(1 * 16) + 0
 #define VStage			beta
-#define VRelease		4
-#define VShortString	"2.1b4"
+#define VRelease		5
+#define VShortString	"2.1b5"
 
 #define	CopyrightString	"©2000-2001 J.-P. Stierlin"
