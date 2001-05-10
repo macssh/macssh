c2fe8c35e69efd3b8f1f12035f3fe28089702031
chombier <>
2001-05-10 14:43:17 +0000

    added japanese translation

---
+++ macssh/source/parse/translate.h
@@ -0,0 +1,16 @@
+
+#define kTRJIS			-1
+#define kTREUC_JP		-2
+#define kTShiftJIS		-3
+
+#define JISX0212		-100
+
+/* character sets for ISO-2022-JP (kTRJIS) */
+enum {
+    kASCII, 
+    kJISX0201_1976,
+    kJISX0208_1978,
+    kJISX0208_1983,
+    kJISX0201_1976Kana,                         // [Not Std ISO-2022-JP]
+    kJISX0212_1990                              // [Not Std ISO-2022-JP]
+};
