d2fdcd48a211ada0ac5538991263f5b344d87666
chombier <>
2001-06-18 09:12:22 +0000

    aded printscreen

--- macssh/source/printing/printing.proto.h
+++ macssh/source/printing/printing.proto.h
@@ -5,7 +5,11 @@ short userabort(void);
 void printGraph(short dnum);
 void printPages(TPPrPort prPort, THPrint PrPrintRec, Str255 Title, short columns,
 				char **charh, short refNum, short useTitle, short theScreen);
-void printText(short vs, Str255 Title, short scrn);
+void printText(short vs, char **charh, Str255 Title, short scrn);
+void printScreen(short vs, Str255 Title, short scrn);
+void printSel(short vs, Str255 Title, short scrn);
+
 void PrintPageSetup(void);
+void PrintScreen(void);
 void PrintSelection(void);
 THPrint PrintSetupRecord(void);
