4044a39ed41fb431cf1d71d284ab20cf1d6f1148
Brendan Shanks <brendan@bslabs.net>
2017-07-20 00:09:37 -0700

    Fix redundant typedef errors when compiling against CWPro6 MSL

--- GUSI/include/inttypes.h
+++ GUSI/include/inttypes.h
@@ -3,13 +3,16 @@
 #ifndef _INTTYPES_H_
 #define _INTTYPES_H_
 
+#ifdef __MWERKS__
+/* CodeWarrior had this header since at least Pro 5, but I only noticed this now. */
+#include <stdint.h>
+#else
 /*
  * Regrettably, this is needed for *int64_t
  */
 #include <MacTypes.h>
 #include <ansi_parms.h>
 
-#if __MSL__ < 0x7001
 #if __MSL__ < 0x6000
 typedef char				int8_t;
 #else
@@ -24,8 +27,6 @@ typedef unsigned long		uint32_t;
 typedef UInt64				uint64_t;
 typedef long				intptr_t;
 typedef unsigned long		uintptr_t;
-#else
-#include <stdint.h>
 #endif
 
 #endif /* _INTTYPES_H_ */
